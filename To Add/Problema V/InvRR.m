0b04fd631d3cd59a947377551eec4451eb146820
Pier Francesco Bachini <pier.bachini@gmail.com>
2022-10-19 00:25:47 +0200

    Add documentation to InvRR file

--- To Add/Problema V/InvRR.m
+++ To Add/Problema V/InvRR.m
@@ -1,30 +1,29 @@
-function [rr_vect] = InvRR(rr_vect)
+function [flipped] = InvRR(to_flip)
 
-dim=size(rr_vect,1);
+% Flip vector rows following the sequent structure:
+% 1 2 3 4 -> 3 4 1 2 || 1 2 3 4 5 -> 3 4 5 1 2
+% 
+% [flipped] = InvRR(to_flip)
+% Input argument:
+% -------------------------------------------------
+% to_flip   [nxm]   matrix to flip
+%
+% Output argument:
+% -------------------------------------------------
+% flipped   [nxm]   flipped matrix
 
-% if rem(dim,2)==0
-%     for i=1:dim
-%         if i<= dim/2
-%             dummy(i,:)=rr_vect(dim/2+i,:);
-%         else
-%             dummy(i,:)=rr_vect(i-dim/2,:);
-%         end
-%     end
-% else
-%     error("Ancora da implementare");
-% end
+dim = size(to_flip,1);                              % Input vector rows number 
+dummy = zeros(size(to_flip));                       % Temporary matrix to work on
 
-dummy = zeros(size(rr_vect));
-
-if rem(dim, 2) == 0
-   dummy(1:dim/2, :) = rr_vect(dim/2+1:end, :);
-   dummy(dim/2+1:end, :) = rr_vect(1:dim/2, :);
-else
-    dm = round(dim/2);
-    dummy(1:dm, :) = rr_vect(dm:end, :);
-    dummy(dm+1:end, :) = rr_vect(1:dm-1, :);
+if rem(dim, 2) == 0                                 % Check rows are even
+   dummy(1:dim/2, :) = to_flip(dim/2+1:end, :);
+   dummy(dim/2+1:end, :) = to_flip(1:dim/2, :);
+else                                                % If rows are odd the middle one will be
+    dm = round(dim/2);                              % moved to the top
+    dummy(1:dm, :) = to_flip(dm:end, :);
+    dummy(dm+1:end, :) = to_flip(1:dm-1, :);
 end
 
-rr_vect=dummy;
+flipped = dummy;                                    % Return the flipped matrix
 
 end
