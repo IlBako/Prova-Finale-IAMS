2effe59b5a5e616e9479d44abec541bb30c8871c
SteBe01 <94758984+SteBe01@users.noreply.github.com>
2022-10-21 19:19:06 +0200

    Add files via upload

---
+++ Misc/Semi orbit/semiOrb.m
@@ -0,0 +1,110 @@
+function [rr_vect_new] = semiOrb(rr_vect_complete,theta1,theta2,a, e, i, OM, om, mu)
+
+n=0;
+while theta1>=2*pi
+    theta1=theta1-2*pi;
+    n=n+1;
+end
+theta2=theta2-2*pi*n;
+
+if theta1==0 && theta2==0
+    warning("theta1 = theta2 = 0!");
+end
+
+if theta1>theta2
+    [vect1] = semiOrbInside(rr_vect_complete,theta1,2*pi);
+    vect1=vect1((1:end-1),:);
+    [vect2] = semiOrbInside(rr_vect_complete,0,theta2);
+    rr_vect_new=[vect1; vect2];
+elseif theta2-theta1>2*pi
+    iter=floor((theta2-theta1)/(2*pi));
+    [vect1] = semiOrbInside(rr_vect_complete,theta1,2*pi);
+    vect1=vect1((1:end-1),:);
+    dummy=vect1;
+    for j=1:iter
+        [vect] = semiOrbInside(rr_vect_complete,0,2*pi);
+        vect=vect((1:end-1),:);
+        dummy=[dummy; vect];
+    end
+    [vect2] = semiOrbInside(rr_vect_complete,0,theta2);
+    rr_vect_new=[dummy; vect2];
+else
+    [rr_vect_new] = semiOrbInside(rr_vect_complete,theta1,theta2);
+end
+
+
+
+
+function [vect] = semiOrbInside(rr_vect_complete,theta1,theta2)
+
+    [radius1, ~] = parorb2rv(a, e, i, OM, om, theta1, mu);
+    [radius2, ~] = parorb2rv(a, e, i, OM, om, theta2, mu);
+    
+    stop1=1;
+    i=0;
+    dim=length(rr_vect_complete);
+    if theta1>=pi
+        i=floor(dim/2);
+    end
+    
+    while stop1==1
+        if theta2-theta1<=pi
+            i=i+1;
+            if theta1<pi
+                if norm(rr_vect_complete(i,:))>=norm(radius1)
+                    stop1=0;
+                end
+            else
+                if norm(rr_vect_complete(i,:))<=norm(radius1)
+                    stop1=0;
+                end
+            end
+        else
+            i=i+1;
+            if norm(rr_vect_complete(i,:))>=norm(radius1)
+                stop1=0;
+            end
+        end
+    end
+    
+    
+    if theta2<pi
+        stop=1;
+        k=1;
+        while stop
+            vect(k,:)=rr_vect_complete(k+i-1,:);
+            if norm(vect(k,:))>norm(radius2)
+                stop=0;
+            end
+            k=k+1;
+            if k==ceil(dim/2)+1
+                break
+            end
+        end
+    elseif theta2>pi && theta2<2*pi
+        stop1=1;
+        stop2=1;
+        k=1;
+        while stop1==1 || stop2==1
+            vect(k,:)=rr_vect_complete(k+i-1,:);
+            if norm(vect(k,:))>norm(radius2)
+                stop1=0;
+            end
+            if stop1==0 && norm(vect(k,:))<norm(radius2)
+                stop2=0;
+            end
+            k=k+1;
+            if k==dim+1-i
+                break
+            end
+        end
+    elseif theta2==pi
+        vect=rr_vect_complete((i:ceil(dim/2)),:);
+    else
+        vect=rr_vect_complete((i:dim),:);
+    end
+
+end
+
+
+end
---
+++ Misc/Semi orbit/semiOrb_test.m
@@ -0,0 +1,46 @@
+%% semiOrb test
+
+clear, clc
+close all
+
+mu = 398600;
+a = 13290;
+e = 0.3855;
+i = 0.9528;
+OM = 2.5510;
+om = 2.2540;
+
+T_tot=2*pi*sqrt(a^3/mu);
+T_size=200;
+T_vect=linspace(0,T_tot,T_size+1);
+
+theta_vect=[];
+for k=1:length(T_vect)
+    t=T_vect(k);
+    tp=T_tot/2;
+
+    M=sqrt(mu/(a^3))*(t-tp);
+
+    f=@(x) -M+x-e*sin(x);
+    df=@(x) 1-e.*cos(x);
+
+    [xvect,~]=newton(1,100,1e-8,f,df);
+    E=xvect(end);
+
+    theta=2*atan(tan(E/2)/sqrt((1-e)/(1+e)));
+
+    theta_vect=[theta_vect; theta];
+end
+
+[rr_vect, vv_vect] = mat_parorb2rv(a,e,i,OM,om,theta_vect, mu);
+[rr_vect] = InvRR(rr_vect);
+
+plot3(rr_vect(:,1),rr_vect(:,2),rr_vect(:,3))
+hold on, grid on, grid minor
+
+theta1=1.7*pi;
+theta2=0.6*pi;
+
+[rr_vect_new] = semiOrb(rr_vect,theta1,theta2,a, e, i, OM, om, mu);
+plot3(rr_vect_new(:,1),rr_vect_new(:,2),rr_vect_new(:,3))
+view(120,20);
